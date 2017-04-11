28c7a9f9e73f4f86ed0db78047e1d5a4605a673c
Avinash <akka@usc.edu>
2017-04-11 16:04:20 -0700

    Create MergeSortedLists.java

---
+++ MergeSortedLists.java
@@ -0,0 +1,23 @@
+//Given two Sorted Single Linked Lists, merge and provide a single sorted linked list.
+/*
+
+class ListNode
+{
+  int val;
+  ListNode next;
+
+ListNode(int x)
+{
+  val = x;
+}
+}
+
+*/
+
+class MergeSortedLists
+{
+
+  public 
+
+
+}
