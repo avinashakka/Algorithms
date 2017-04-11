37ea089fbedd0cc00d8d7858a87ba05428ded3d6
Avinash <akka@usc.edu>
2017-04-11 16:19:04 -0700

    Update MergeSortedLists.java

--- MergeSortedLists.java
+++ MergeSortedLists.java
@@ -18,6 +18,76 @@ class MergeSortedLists
 {
 
   public ListNode mergeLists(ListNode l1,ListNode l2)
-
-
+  {
+    ListNode head;
+    ListNode pointer1;
+    ListNode pointer2;
+    ListNode mergePointer;
+    
+    if(l1 == null && l2 == null)
+      return null;
+    
+    if(l1 == null && l2 != null)
+      return l2;
+    
+    if(l2 == null && l1 != null)
+      return l1;
+    
+    pointer1 = l1;
+    pointer2 = l2;
+    mergePointer = head;
+    
+    while(pointer1 != null || pointer2 != null)
+    {
+      if(pointer1.val > pointer2.val)
+      {
+        ListNode temp = new ListNode();
+        temp.val = pointer2.val;
+        temp.next = null;
+        if(mergePointer == null)
+          mergePointer = temp;
+        else
+        {
+          mergePointer.next = temp;
+          mergePointer = temp;
+        }
+        pointer2 = pointer2.next;
+      }
+      else
+      {
+        ListNode temp = new ListNode();
+        temp.val = pointer1.val;
+        temp.next = null;
+        if(mergePointer == null)
+          mergePointer = temp;
+        else
+        {
+          mergePointer.next = temp;
+          mergePointer = temp;
+        }
+        pointer1 = pointer1.next;
+      }
+    }
+    
+   while(pointer1 != null)
+   {
+     ListNode temp = new ListNode();
+     temp.val = pointer1.val;
+     temp.next = null;
+     mergePointer.next = temp;
+     mergePointer = temp;
+     pointer1 = pointer1.next;
+   }
+    
+   while(pointer2 != null)
+   {
+     ListNode temp = new ListNode();
+     temp.val = pointer2.val;
+     temp.next = null;
+     mergePointer.next = temp;
+     mergePointer = temp;
+     pointer2 = pointer2.next;
+   }
+    return head;
+  }
 }
